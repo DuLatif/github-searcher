11da0547c5ea95206de8051a7b22c906e71f0ec1
Abdul Latif Mubasir <bj.angel119@gmail.com>
2024-05-25 13:02:57 +0700

    config: install sass and add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          "@import 'src/themes/colors.scss';@import 'src/themes/spacing.scss';@import 'src/themes/breakpoints.scss';@import 'src/themes/typography.scss';",
+      },
+    },
+  },
+});
