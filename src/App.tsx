95bfc7afddab726555977659ee07350a06ecf150
Abdul Latif Mubasir <bj.angel119@gmail.com>
2024-05-26 22:19:25 +0700

    refactor: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home";
 import styles from "@/themes/global.module.scss";
+
+const HomePage = lazy(() => import("./pages/home"));
 
 function App() {
   return (
     <div className={styles.Container}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
